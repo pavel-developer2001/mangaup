3868726fa579d348f72d180ee530e8996cf748f2
pavel-developer2001 <gaidukpavel18@gmail.com>
2020-12-14 15:25:57 +0300

    add footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SliderList from '../components/SliderList'
 import Update from '../components/Update'
+import Footer from '../components/Footer'
 
 const Home = () => {
     return (
         <div className="home">
             <SliderList />
             <Update />
+            <Footer />
         </div>
     )
 }
